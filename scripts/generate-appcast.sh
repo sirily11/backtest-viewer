2130c4bb3d91172b5bebd290fe786caab202a627
sirily11 <32106111+sirily11@users.noreply.github.com>
2025-03-19 22:58:58 +0800

    fix: update configuration

--- scripts/generate-appcast.sh
+++ scripts/generate-appcast.sh
@@ -6,6 +6,5 @@ echo "Generating appcast for version: $VERSION"
 # Generate appcast.xml
 ./bin/generate_appcast ./\
  --ed-key-file sparkle.key \
- --link https://github.com/trading-analyzer/trading-analyzer/releases \
- --download-url-prefix https://github.com/trading-analyzer/trading-analyzer/releases/download/${VERSION}/ \
- --channel production
+ --link https://github.com/sirily11/backtest-viewer/releases \
+ --download-url-prefix https://github.com/sirily11/backtest-viewer/releases/download/${VERSION}/
---
+++ trading-analyzer/Services/SparkleService.swift
@@ -0,0 +1,15 @@
+//
+//  SparkleService.swift
+//  trading-analyzer
+//
+//  Created by Qiwei Li on 3/19/25.
+//
+import Foundation
+import Sparkle
+
+class UpdaterDelegate: NSObject, SPUUpdaterDelegate {
+    deinit {
+        print("UpdaterDelegate deinit")
+    }
+    // SPUUpdaterDelegate methods
+}
